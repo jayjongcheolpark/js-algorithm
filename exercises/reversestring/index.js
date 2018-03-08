26106b12fbda78afc8dbe1f1f3e17ae2f3480cb9
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-08 16:35:29 -0500

    complete reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//   let reversed = ''
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//   return reversed
+// }
+
 function reverse(str) {
-  return str.split('').reverse().join('')
+  return str.split('').reduce((rev, char) => char + rev, '')
 }
 
 module.exports = reverse
