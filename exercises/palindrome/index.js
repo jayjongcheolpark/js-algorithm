ebdd787317512442d33d4d1778c32d187caf24ab
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-08 17:12:25 -0500

    complete palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   return str.split('').reverse().join('') === str
+// }
 
-module.exports = palindrome;
+function palindrome(str) {
+  return str.split('').every((char, i) => char === str[str.length - i - 1])
+}
+
+module.exports = palindrome
