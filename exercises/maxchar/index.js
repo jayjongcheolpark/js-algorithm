bf864d105b00789f9479ea89db279fb710919862
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-09 21:34:11 -0500

    complete maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {}
+  let max = 0
+  let maxCh = ''
+  for (const char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
 
-module.exports = maxChar;
+  for (const char in charMap) {
+    console.log(char)
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxCh = char
+    }
+  }
+  return maxCh
+}
+
+module.exports = maxChar
