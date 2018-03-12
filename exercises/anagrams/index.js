8832ac4773bcbfa92889a219082645b57d728298
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-11 20:39:40 -0400

    anagrams solution #1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function buildCharMap(str) {
+  const charMap = {}
 
-module.exports = anagrams;
+  for (const char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
+
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA)
+  const bCharMap = buildCharMap(stringB)
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false
+  }
+
+  for (const char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+module.exports = anagrams
