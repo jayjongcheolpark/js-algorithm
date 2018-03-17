0bd76804c5365124828208e33b78936adb435033
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-17 19:17:31 -0400

    steps solution #1

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,19 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = ''
+
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += '#'
+      } else {
+        stair += ' '
+      }
+    }
+    console.log(stair)
+  }
+}
 
 module.exports = steps;
