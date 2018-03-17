4264403f984d45944910b088896e68108bb4ea68
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-17 19:28:20 -0400

    steps solution #2

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = ''
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = ''
 
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
-    console.log(stair)
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return
   }
+
+  if (n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1 )
+  }
+
+  if (stair.length <= row) {
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+  steps(n, row, stair)
 }
 
 module.exports = steps;
