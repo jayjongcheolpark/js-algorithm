44fe3dfd22fe9f27863b75fb25777ca35b25ab29
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-18 15:31:39 -0400

    refactoring steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -42,12 +42,8 @@ function steps(n, row = 0, stair = '') {
     return steps(n, row + 1 )
   }
 
-  if (stair.length <= row) {
-    stair += '#'
-  } else {
-    stair += ' '
-  }
-  steps(n, row, stair)
+  const add = stair.length <= row ? '#' : ' '
+  steps(n, row, stair + add)
 }
 
 module.exports = steps;
